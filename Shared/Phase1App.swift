489c3a31ef5dc6c27e7c2ca3276bdcd5e9b4886f
ChienLe <chienle@tdt.asia>
2021-12-14 10:38:13 +0700

    Initial Commit

---
+++ Shared/Phase1App.swift
@@ -0,0 +1,17 @@
+//
+//  Phase1App.swift
+//  Shared
+//
+//  Created by Le Huu Chien on 14/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct Phase1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
